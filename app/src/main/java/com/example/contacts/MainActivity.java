9a298dc78987e125238b40f21e30f515c24ad154
DESHAPRIYA <deshapriyakhatua@gmail.com>
2022-10-07 21:42:14 +0530

    Update MainActivity.java
    07-10-22

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //hiding menu bar
         getSupportActionBar().hide();
 
         ArrayList<ContactsList> dataList = new ArrayList<>();
